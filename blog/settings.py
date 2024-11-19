e9625b73081f01c8877280a330e8459a5aa6689d
Julio Cesar Hernández Pedraza <juliohp1995@gmail.com>
2024-11-19 19:06:19 +0100

    update

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "FALSE").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost", "127.0.0.1", "elblog.onrender.com").split(" ")
 
 
 
