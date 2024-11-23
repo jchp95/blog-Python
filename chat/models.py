9af3184ad9ea2dfa4c220bd737aaf0c65480a9c4
Julio Cesar Hernández Pedraza <juliohp1995@gmail.com>
2024-11-23 17:48:04 +0100

    cosas nuevas

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# models.py
+from django.db import models
+
+class PalabraFrecuente(models.Model):
+    palabra = models.CharField(max_length=100)
+    frecuencia = models.IntegerField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.palabra
